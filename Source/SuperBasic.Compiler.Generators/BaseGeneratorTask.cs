bb6d3ea8823ed1fe339d5bf4d7d9d1c4b17de035
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2018-08-07 17:17:56 -0700

    Generating TokenKinds

---
+++ Source/SuperBasic.Compiler.Generators/BaseGeneratorTask.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Build.Utilities;
+using SuperBasic.Utilities;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace SuperBasic.Compiler.Generators
+{
+    public abstract class BaseGeneratorTask<TModel> : Task
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+
+        protected abstract string GenerateDocumentContents(TModel model);
+
+        public override bool Execute()
+        {
+            using (var stream = new FileStream(this.Input, FileMode.Open))
+            {
+                var settings = new XmlReaderSettings();
+                settings.ValidationEventHandler += (sender, args) =>
+                {
+                    switch (args.Severity)
+                    {
+                        case XmlSeverityType.Error:
+                            this.Log.LogError(args.Message);
+                            break;
+                        case XmlSeverityType.Warning:
+                            this.Log.LogWarning(args.Message);
+                            break;
+                        default:
+                            throw ExceptionUtilities.UnexpectedValue(args.Severity);
+                    }
+                };
+
+                using (var xmlReader = XmlReader.Create(stream, settings))
+                {
+                    var serializer = new XmlSerializer(typeof(TModel));
+                    var model = (TModel)serializer.Deserialize(xmlReader);
+
+                    File.WriteAllText(this.Output, this.GenerateDocumentContents(model));
+                }
+            }
+
+            return !this.Log.HasLoggedErrors;
+        }
+    }
+}
---
+++ Source/SuperBasic.Utilities/ExceptionUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuperBasic.Utilities
+{
+    internal static class ExceptionUtilities
+    {
+        public static InvalidOperationException UnexpectedValue<TValue>(TValue value)
+        {
+            return new InvalidOperationException($"Unexpected value '{value}' of type '{typeof(TValue).FullName}'");
+        }
+    }
+}
