c663bae627b639111b886c753df8ed1dde5cb815
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2018-08-12 16:08:37 -0700

    Added diagnostics and analyzers

---
+++ Source/SuperBasic.Compiler/Syntax/Models/TextPosition.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="TextPosition.cs" company="2018 Omar Tawfik">
+// Copyright (c) 2018 Omar Tawfik. All rights reserved. Licensed under the MIT License. See LICENSE file in the project root for license information.
+// </copyright>
+
+namespace SuperBasic.Compiler.Syntax
+{
+    internal readonly struct TextPosition
+    {
+        public short Line { get; }
+
+        public short Column { get; }
+    }
+}
---
+++ Source/SuperBasic.Utilities/StringUtilities.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="StringUtilities.cs" company="2018 Omar Tawfik">
+// Copyright (c) 2018 Omar Tawfik. All rights reserved. Licensed under the MIT License. See LICENSE file in the project root for license information.
+// </copyright>
+
+namespace SuperBasic.Utilities
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    public static class StringUtilities
+    {
+        public static string Join(this IEnumerable<string> enumerable, string separator) => string.Join(separator, enumerable);
+
+        public static string ToDisplayString(this string value) => value;
+
+        public static string ToDisplayString(in this char value) => value.ToString(CultureInfo.CurrentCulture);
+    }
+}
