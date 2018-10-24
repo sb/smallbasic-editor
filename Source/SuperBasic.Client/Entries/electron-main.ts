cc7649755ad5494f20abd6f6859aa991844c8486
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2018-10-23 23:37:33 -0700

    Interop Sample now working with json serialization

---
+++ Source/SuperBasic.Client/Entries/electron-main.ts
@@ -0,0 +1,34 @@
+﻿/*!
+ * Copyright (c) 2018 Omar Tawfik. All rights reserved. Licensed under the MIT License. See LICENSE file in the project root for license information.
+ */
+
+import { app, BrowserWindow } from "electron";
+
+let window: BrowserWindow | null = null;
+
+function createWindow(): void {
+    window = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    window.loadFile("index.html");
+
+    window.on("closed", () => {
+        window = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (window === null) {
+        createWindow();
+    }
+});
---
+++ Source/SuperBasic.Client/Entries/electron-renderer.ts
@@ -0,0 +1,6 @@
+﻿/*!
+ * Copyright (c) 2018 Omar Tawfik. All rights reserved. Licensed under the MIT License. See LICENSE file in the project root for license information.
+ */
+
+import "../Bridge/Bridge.Generated";
+import "./web";
