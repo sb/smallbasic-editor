d882db3e0fb4ea368c801ae4349d1b19a5c20e5f
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2019-04-25 00:22:19 -0700

    Bind case-insensitive libraries types and members
    Fixes #70

--- Source/SmallBasic.Tests/Runtime/LibrariesTests.cs
+++ Source/SmallBasic.Tests/Runtime/LibrariesTests.cs
@@ -44,5 +44,57 @@ y = Text.GetSubTextToEnd(x, {start})
 x = 1234567890
 y = {result}");
         }
+
+        [Fact]
+        public Task ItEvaluatesDifferentCasingOfLibraryTypes()
+        {
+            return new SmallBasicCompilation(@"
+x = MATH.Min(1, 5)
+y = math.Min(2, -6)
+z = maTH.Min(8, 9)
+").VerifyRealRuntime(@"
+x = 1
+y = -6
+z = 8");
+        }
+
+        [Fact]
+        public Task ItEvaluatesDifferentCasingOfLibraryMethods()
+        {
+            return new SmallBasicCompilation(@"
+x = Math.MIN(1, 5)
+y = Math.min(2, -6)
+z = Math.MiN(8, 9)
+").VerifyRealRuntime(@"
+x = 1
+y = -6
+z = 8");
+        }
+
+        [Fact]
+        public Task ItEvaluatesDifferentCasingOfLibraryProperties()
+        {
+            return new SmallBasicCompilation(@"
+x = Math.PI
+y = Math.pi
+z = Math.pI
+").VerifyRealRuntime(@"
+x = 3.14159265358979
+y = 3.14159265358979
+z = 3.14159265358979");
+        }
+
+        [Fact]
+        public void ItEvaluatesDifferentCasingOfLibraryEvents()
+        {
+            new SmallBasicCompilation(@"
+Sub foo
+EndSub
+
+GraphicsWindow.KEYDOWN = foo
+GraphicsWindow.keyup = foo
+GraphicsWindow.mouseMOVE = foo
+").VerifyDiagnostics();
+        }
     }
 }
